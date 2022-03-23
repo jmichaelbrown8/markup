b09cef9ca786c527b519b98bd95ba9c7577c66fc
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-22 18:23:00 -0700

    Replaced all references to template app, including logo, to new branding and made webpack config work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,21 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("markupDB", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("markup")) {
+        console.log("markup store already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("markup", { keyPath: "id", autoIncrement: true });
+      console.log("markup store created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
