4a90cf4b88a6412205fb84082fff0febc430eda0
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-24 21:05:56 -0700

    Fixed the show/hide for button install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,60 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Check onload to see if the app is supported
+window.addEventListener("load", () => {
+  if (!installSupported()) {
+    console.log("Install is not supported");
+    hideInstallButton();
+    return;
+  }
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  console.log("Install is supported");
+  // Handle install available
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Store the triggered events
+    window.deferredPrompt = event;
+    showInstallButton(event);
+  });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Handle user request to install
+  butInstall.addEventListener("click", async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+      return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+  });
+
+  // Hide the install button
+  window.addEventListener("appinstalled", (event) => {
+    hideInstallButton();
+
+    // Clear prompt
+    window.deferredPrompt = null;
+  });
+});
+
+/** Checks to see if install is supported in browser */
+function installSupported() {
+  return "getInstalledRelatedApps" in navigator;
+}
+
+/** Show the install button */
+function showInstallButton(event) {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
+  // divInstallStatus.textContent = 'true';
+  // btnAdd.removeAttribute('disabled');
+}
+
+/** Hide the install button */
+function hideInstallButton() {
+  // add the hidden class to the button.
+  butInstall.classList.toggle("hidden", true);
+}
